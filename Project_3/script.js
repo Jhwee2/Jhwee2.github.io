2134398724cdd71ffc09084135dde90cc6f498c8
anthonypoon12 <anthony@poons.us>
2022-01-24 17:33:21 -0500

    fixed reset button

--- Project_3/script.js
+++ Project_3/script.js
@@ -6,16 +6,18 @@ document.getElementById("highscore").innerHTML=highscore;
 var inputfrompage = document.querySelector('input');
 const btn = document.getElementById('check');
 btn.addEventListener('click',func);
-const randnum = Math.floor(Math.random()*100 + 1);
+const resbtn = document.getElementById('reset');
+resbtn.addEventListener('click',reset);
+var randnum = Math.floor(Math.random()*100 + 1);
 console.log("Random number is: " + randnum);
 //input is string
 function func(){
     input = inputfrompage.value;
     if (score!==0){
         if (input==="")
-            alert("no input");
+        alert("no input");
         else if (isNaN(parseInt(input)))
-            alert("invalid value");
+        alert("invalid value");
         else{
             var innum = parseInt(input);
             if (innum > randnum){
@@ -25,19 +27,27 @@ function func(){
             else if (innum <randnum){
                 score--;
                 if (score!==0)
-                    alert("Too low");
+                alert("Too low");
             }
             else{
                 document.body.style.backgroundColor = "red";
                 //Still need to add the showing of secret number
                 //Need to change background color/dimensions of box of secret number
                 if (score>highscore)
-                    highscore=score;
-                    document.getElementById("highscore").innerHTML=highscore;
+                highscore=score;
+                document.getElementById("highscore").innerHTML=highscore;
             }
         }
     }
     document.getElementById("score").innerHTML=score;
     if(score===0)
-        alert("Sorry, the game is over");
+    alert("Sorry, the game is over");
+}
+function reset(){
+    document.body.style.background = "";
+    score = 10;
+    document.getElementById("score").innerHTML=score;
+    randnum = Math.floor(Math.random()*100 + 1);
+    console.log("Random number is: " + randnum);
+    
 }
